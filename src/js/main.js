fb03dc43afb48bd1541368fe7793e10ed1f15a7b
NuKamil <kamill1983@gmail.com>
2020-04-19 18:26:39 +0200

    Working with js logic

--- src/js/main.js
+++ src/js/main.js
@@ -2,21 +2,34 @@
 
 // service worker registration - remove if you're not going to use it
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function () {
+        navigator.serviceWorker.register("serviceworker.js").then(
+            function (registration) {
+                // Registration was successful
+                console.log(
+                    "ServiceWorker registration successful with scope: ",
+                    registration.scope
+                );
+            },
+            function (err) {
+                // registration failed :(
+                console.log("ServiceWorker registration failed: ", err);
+            }
+        );
     });
-  });
 }
 
 // place your code below
 
+const buttonPlus = document.querySelector(".button_plus-js");
+const buttonMinus = document.querySelector(".button_minus-js");
+const counter = document.querySelector(".licznik-js");
 
-console.log(`Hello world!`);
-
-
+buttonPlus.addEventListener("click", () => {
+    counter.va
+    console.log(counter);
+});
+buttonMinus.addEventListener("click", () => {
+    console.log("baran");
+});
